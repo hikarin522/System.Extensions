7eab41e39d55417f0d356b43931293a6d07252ff
hikari <hikarin522@outlook.jp>
2019-02-20 03:54:44 +0900

    feat: add BitVector32Extensions

---
+++ System.Extensions/Collections/BitVector32Extensions.cs
@@ -0,0 +1,14 @@
+#if AFTER_NETSTANDARD2_0 || AFTER_NET45
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+
+namespace System.Linq
+{
+    public static class BitVector32Extensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static BitVector32 ToBitVector32(this int data)
+            => new BitVector32(data);
+    }
+}
+#endif
