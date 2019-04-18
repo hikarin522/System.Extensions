3dce0577932d6c15bfac1a69fc405db8d5b16eda
hikari <hikarin522@outlook.jp>
2019-04-18 13:06:35 +0900

    add [Reflection] GetMethod

--- stepstool/Reflection/ReflectionExtensions.cs
+++ stepstool/Reflection/ReflectionExtensions.cs
@@ -13,5 +13,11 @@ namespace System.Reflection
         public static T CreateDelegate<T>(this MethodInfo method, object target)
             where T : Delegate
             => (T)method.CreateDelegate(typeof(T), target);
+
+#if NET || AFTER_NETSTANDARD2_0
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static MethodInfo GetMethod(this Type type, string name, BindingFlags bindingAttr, params Type[] types)
+            => type.GetMethod(name, bindingAttr, null, types ?? Type.EmptyTypes, null);
+#endif
     }
 }
